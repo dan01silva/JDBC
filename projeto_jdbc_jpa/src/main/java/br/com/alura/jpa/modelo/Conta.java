392684d049c140d4c4b349284ec5dc38746895ab
danms <danms@DESKTOP-FAULAFQ.lan>
2021-02-09 02:51:46 -0300

    JPA = ORM (object Relacional Mapper) Inicalianzando JPA configurando persistence.xml EntityManegerFactory

---
+++ projeto_jdbc_jpa/src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -0,0 +1,42 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Integer id;
+	private Integer agencia;
+	private Integer numero;
+	private String titular;
+	
+	public Conta() {
+	}
+
+	public Conta(Integer agencia, Integer numero, String titular) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.titular = titular;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getAgencia() {
+		return agencia;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+}
